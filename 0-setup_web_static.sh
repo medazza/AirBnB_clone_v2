10c433037f023e4a9b3b412ee5958709058dccc8
Mohamed AZZA <118252819+medazza@users.noreply.github.com>
2024-02-04 09:29:26 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
 
-
-# Create necessary folders
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "Hello, this is a test file" | sudo tee /data/web_static/releases/test/index.html
-
-# Create symbolic link
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-
-# Set ownership
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-config="location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n"
-sudo sed -i "/^\tlocation \/ {/a $config" /etc/nginx/sites-available/default
-
-# Restart Nginx
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
 
 exit 0
