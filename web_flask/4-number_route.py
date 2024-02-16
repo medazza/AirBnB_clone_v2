ff5b87f3c3cfeb17b31b15f1913fcc54563446c8
medazza <midazza27@gmail.com>
2024-02-16 11:39:29 -0800

    task 4 web flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """  display “Hello HBNB!” """
+    """ display “Hello HBNB!” """
     return "Hello HBNB!"
 
 
@@ -26,9 +26,15 @@ def c_hbnb(text):
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_hbnb(text="is cool"):
-    """ display “Python ”, followed by the value of the text """
+    """ display “Python ”, followed by the value of the text variable """
     text = text.replace('_', ' ')
     return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_hbnb(n):
+    """ display “n is a number” only if n is an integer """
+    return f'{n} is a number'
 
 
 if __name__ == '__main__':
