0bce0591dc3085654e7384e4f58d8bb939c9b551
medazza <midazza27@gmail.com>
2024-02-17 02:30:21 -0800

    task 5 web flask update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def number_hbnb(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_temp_hbnb(n):
     """ display a HTML page only if n is an integer """
-    return render_template('5-number.html', value=n)
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
