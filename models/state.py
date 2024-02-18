6d157bc0d319d34027612f9f762c973609980fe9
medazza <midazza27@gmail.com>
2024-02-18 00:28:38 -0800

    task 7 web flask

--- models/state.py
+++ models/state.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, Integer, String
 import models
 from models.city import City
 import shlex
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -16,21 +17,22 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
 
-    @property
-    def cities(self):
-        """getter for list of city instances related to the state"""
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade='all, delete', backref="state")
+    else:
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            var = models.storage.all()
+            lista = []
+            result = []
+            for key in var:
+                city = key.replace('.', ' ')
+                city = shlex.split(city)
+                if (city[0] == 'City'):
+                    lista.append(var[key])
+            for elem in lista:
+                if (elem.state_id == self.id):
+                    result.append(elem)
+            return (result)
